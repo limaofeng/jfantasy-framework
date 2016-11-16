2056f78534f7d1d7bcc4bafa20178377705f2b5a
李茂峰 <limaofeng@msn.com>
2016-11-16 13:05:47 +0800

    hibernate 监听注册方式修改

--- filestore/JavaSource/org/jfantasy/filestore/listener/FileManagerEventListener.java
+++ filestore/JavaSource/org/jfantasy/filestore/listener/FileManagerEventListener.java
@@ -1,60 +1,42 @@
 package org.jfantasy.filestore.listener;
 
+import org.hibernate.event.spi.EventType;
+import org.hibernate.event.spi.PostDeleteEvent;
+import org.hibernate.event.spi.PostInsertEvent;
+import org.hibernate.event.spi.PostUpdateEvent;
 import org.jfantasy.filestore.bean.FileManagerConfig;
 import org.jfantasy.filestore.service.FileManagerFactory;
-import org.jfantasy.framework.dao.annotations.EventListener;
-import org.jfantasy.framework.util.common.ObjectUtil;
-import org.hibernate.event.spi.*;
-import org.hibernate.persister.entity.EntityPersister;
-
+import org.jfantasy.framework.dao.hibernate.listener.AbstractChangedListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 @Component
-@EventListener({"post-insert", "post-update", "post-delete"})
-public class FileManagerEventListener implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
+public class FileManagerEventListener extends AbstractChangedListener<FileManagerConfig> {
 
     private static final long serialVersionUID = 1082020263270806626L;
 
     @Autowired
     private FileManagerFactory factory;
 
-    public static boolean hasModified(PostUpdateEvent event, String property) {
-        Arrays.binarySearch(event.getPersister().getPropertyNames(), property);
-        int index = ObjectUtil.indexOf(event.getPersister().getPropertyNames(), property);
-        return index != -1 && event.getState()[index].equals(event.getOldState()[index]);
+    public FileManagerEventListener() {
+        super(EventType.POST_INSERT, EventType.POST_UPDATE, EventType.POST_DELETE);
     }
 
     @Override
-    public void onPostDelete(PostDeleteEvent event) {
-        if (event.getEntity() instanceof FileManagerConfig) {
-            factory.remove((FileManagerConfig) event.getEntity());
-        }
+    public void onPostDelete(FileManagerConfig config, PostDeleteEvent event) {
+        factory.remove(config);
     }
 
     @Override
-    public void onPostUpdate(PostUpdateEvent event) {
-        if (event.getEntity() instanceof FileManagerConfig) {
-            FileManagerConfig config = (FileManagerConfig) event.getEntity();
-            if (hasModified(event, "configParamStore")) {
-                factory.registerFileManager(config.getId(), config.getType(), config.getConfigParams());
-            }
-        }
-    }
-
-    @Override
-    public void onPostInsert(PostInsertEvent event) {
-        if (event.getEntity() instanceof FileManagerConfig) {
-            FileManagerConfig config = (FileManagerConfig) event.getEntity();
+    public void onPostUpdate(FileManagerConfig config, PostUpdateEvent event) {
+        if (modify(event, "configParamStore")) {
             factory.registerFileManager(config.getId(), config.getType(), config.getConfigParams());
         }
     }
 
     @Override
-    public boolean requiresPostCommitHanding(EntityPersister persister) {
-        return false;
+    public void onPostInsert(FileManagerConfig config, PostInsertEvent event) {
+        factory.registerFileManager(config.getId(), config.getType(), config.getConfigParams());
     }
 
 }
