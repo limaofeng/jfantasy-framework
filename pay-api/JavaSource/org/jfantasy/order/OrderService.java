68b2a241eb8fc10519dd45a534e15597f77a8d0c
李茂峰 <limaofeng@msn.com>
2016-11-24 19:08:47 +0800

    添加订单退款接口

--- pay-api/JavaSource/org/jfantasy/order/OrderService.java
+++ pay-api/JavaSource/org/jfantasy/order/OrderService.java
@@ -3,6 +3,8 @@ package org.jfantasy.order;
 
 import org.jfantasy.order.entity.Order;
 
+import java.math.BigDecimal;
+
 /**
  * 支付订单接口<br/>
  * 服务接入方需要实现该接口
@@ -24,6 +26,18 @@ public interface OrderService {
      * @return Order
      */
     Order get(String type, String sn);
+
+    /**
+     * 订单退款
+     *
+     * @param type         业务订单 type
+     * @param sn           业务订单 sn
+     * @param refundAmount 退款金额
+     * @param note         备注
+     * @return Order
+     */
+    Order refund(String type, String sn, BigDecimal refundAmount, String note);
+
 
     /**
      * 发起订单关闭操作
