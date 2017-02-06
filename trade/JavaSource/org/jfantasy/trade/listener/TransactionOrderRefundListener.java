c4b78825e65d33c0b327fd36f6f46ac4017befde
李茂峰 <limaofeng@msn.com>
2017-02-06 15:37:26 +0800

    订单退款逻辑修复

--- trade/JavaSource/org/jfantasy/trade/listener/TransactionOrderRefundListener.java
+++ trade/JavaSource/org/jfantasy/trade/listener/TransactionOrderRefundListener.java
@@ -2,12 +2,7 @@ package org.jfantasy.trade.listener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.criterion.Restrictions;
-import org.jfantasy.framework.util.common.ObjectUtil;
-import org.jfantasy.pay.bean.Payment;
-import org.jfantasy.pay.bean.enums.PaymentStatus;
-import org.jfantasy.pay.service.PaymentService;
-import org.jfantasy.pay.service.RefundService;
+import org.jfantasy.pay.service.PayService;
 import org.jfantasy.trade.bean.Project;
 import org.jfantasy.trade.bean.Transaction;
 import org.jfantasy.trade.bean.enums.TxStatus;
@@ -15,8 +10,6 @@ import org.jfantasy.trade.event.TransactionAddedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * 退款处理
@@ -26,32 +19,19 @@ public class TransactionOrderRefundListener implements ApplicationListener<Trans
 
     private static final Log LOG = LogFactory.getLog(TransactionOrderRefundListener.class);
 
-    private RefundService refundService;
-    private PaymentService paymentService;
+    private PayService payService;
 
     @Override
     public void onApplicationEvent(TransactionAddedEvent event) {
         Transaction transaction = event.getTransaction();
         if (event.getStatus() == TxStatus.unprocessed && Project.REFUND.equals(transaction.getProject())) {
-            String id = transaction.get(Transaction.ORDER_ID);
-            List<Payment> payments = paymentService.find(Restrictions.eq("order.id", id));
-            Payment payment = ObjectUtil.find(payments, "status", PaymentStatus.success);
-            if (payment == null) {
-                LOG.error(" 订单可能未支付成功或者已经退款! ");
-                return;
-            }
-            refundService.create(payment, transaction.getAmount(), transaction, "退款");
+            payService.refund(transaction);
         }
     }
 
     @Autowired
-    public void setRefundService(RefundService refundService) {
-        this.refundService = refundService;
-    }
-
-    @Autowired
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
+    public void setPayService(PayService payService) {
+        this.payService = payService;
     }
 
 }
