8c9d937a258b402ff3e9f8c8de8ae8897cbf0ebf
李茂峰 <limaofeng@msn.com>
2017-02-24 12:46:48 +0800

    交易密码验证问题

--- trade/JavaSource/org/jfantasy/trade/service/AccountService.java
+++ trade/JavaSource/org/jfantasy/trade/service/AccountService.java
@@ -168,7 +168,7 @@ public class AccountService {
             if (StringUtil.isBlank(password)) {
                 throw new ValidationException("支付密码不能为空");
             }
-            if (!passwordEncoder.matches(password,from.getPassword())) {
+            if (!passwordEncoder.matches(from.getPassword(),password)) {
                 throw new ValidationException("支付密码错误");
             }
         }
