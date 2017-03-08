eae506786aab6a61d9bc17ddeb9a1b7ad7f677dd
李茂峰 <limaofeng@msn.com>
2017-03-08 17:43:23 +0800

    添加 newInstance 测试类

---
+++ core/test/src/org/jfantasy/framework/util/reflect/FastClassesTest.java
@@ -0,0 +1,13 @@
+package org.jfantasy.framework.util.reflect;
+
+import org.jfantasy.framework.dao.mybatis.interceptors.MultiDataSourceInterceptor;
+import org.junit.Test;
+
+public class FastClassesTest {
+
+    @Test
+    public void newInstance() throws Exception {
+        FastClasses fastClasses = new FastClasses(MultiDataSourceInterceptor.class);
+    }
+
+}
