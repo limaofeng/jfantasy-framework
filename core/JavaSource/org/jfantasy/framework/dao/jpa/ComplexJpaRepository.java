32152475fef06521119e1a90733d7ce8a19278be
李茂峰 <limaofeng@msn.com>
2019-04-03 14:20:50 +0800

    findPager(Pager<T> pager, List<PropertyFilter> filters) 排序修复

--- core/JavaSource/org/jfantasy/framework/dao/jpa/ComplexJpaRepository.java
+++ core/JavaSource/org/jfantasy/framework/dao/jpa/ComplexJpaRepository.java
@@ -2,10 +2,6 @@ package org.jfantasy.framework.dao.jpa;
 
 import org.jfantasy.framework.dao.Pager;
 import org.jfantasy.framework.dao.hibernate.PropertyFilter;
-import org.jfantasy.framework.util.common.BeanUtil;
-import org.jfantasy.framework.util.common.ClassUtil;
-import org.jfantasy.framework.util.common.ObjectUtil;
-import org.jfantasy.framework.util.ognl.OgnlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,10 +9,8 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,10 +28,7 @@ public class ComplexJpaRepository<T, ID extends Serializable> extends SimpleJpaR
 
     @Override
     public Pager<T> findPager(Pager<T> pager, List<PropertyFilter> filters) {
-        PageRequest pageRequest = PageRequest.of(pager.getCurrentPage() - 1, pager.getPageSize());
-        Page<T> page = this.findAll(new PropertyFilterSpecification(filters), pageRequest);
-        pager.reset(Long.valueOf(page.getTotalElements()).intValue(), page.getContent());
-        return pager;
+        return this.findPager(pager, new PropertyFilterSpecification(filters));
     }
 
     @Override
