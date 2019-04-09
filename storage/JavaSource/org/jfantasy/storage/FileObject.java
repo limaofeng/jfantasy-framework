83dc90906ba17012189470a3921aaea0f78d359e
李茂峰 <limaofeng@msn.com>
2019-04-09 18:52:04 +0800

    遗漏提交

---
+++ storage/JavaSource/org/jfantasy/storage/FileObject.java
@@ -0,0 +1,25 @@
+package org.jfantasy.storage;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author limaofeng
+ * @version V1.0
+ * @Description: TODO
+ * @date 2019-04-09 18:15
+ */
+@Data
+public class FileObject {
+    private String id;
+    private String md5;
+    private String name;
+    private String path;
+    private String mimeType;
+    private Long length;
+    private String destination;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
---
+++ storage/JavaSource/org/jfantasy/storage/converter/FileObjectConverter.java
@@ -0,0 +1,32 @@
+package org.jfantasy.storage.converter;
+
+import org.jfantasy.framework.jackson.JSON;
+import org.jfantasy.framework.util.common.StringUtil;
+import org.jfantasy.storage.FileObject;
+
+import javax.persistence.AttributeConverter;
+
+/**
+ * @author limaofeng
+ * @version V1.0
+ * @Description: TODO
+ * @date 2019-04-09 18:17
+ */
+public class FileObjectConverter implements AttributeConverter<FileObject, String> {
+
+    @Override
+    public String convertToDatabaseColumn(FileObject attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        return JSON.serialize(attribute);
+    }
+
+    @Override
+    public FileObject convertToEntityAttribute(String dbData) {
+        if (StringUtil.isBlank(dbData)) {
+            return null;
+        }
+        return JSON.deserialize(dbData, FileObject.class);
+    }
+}
