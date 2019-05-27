303f040a74deeaa8fd2a1ffbb600e6272b86ccc3
李茂峰 <limaofeng@msn.com>
2019-05-27 15:06:17 +0800

    为 LoginUser 添加扩展属性

--- core/JavaSource/org/jfantasy/framework/security/LoginUser.java
+++ core/JavaSource/org/jfantasy/framework/security/LoginUser.java
@@ -1,12 +1,16 @@
 package org.jfantasy.framework.security;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author limaofeng
@@ -56,4 +60,29 @@ public class LoginUser implements Principal {
      * 权限
      */
     private List<String> authoritys;
+    /**
+     *
+     */
+    private Map<String, Object> data;
+
+    @JsonAnySetter
+    public void set(String key, Object value) {
+        if (this.data == null) {
+            this.data = new HashMap<>();
+        }
+        this.data.put(key, value);
+    }
+
+    public <T> T get(String key) {
+        if (this.data == null) {
+            this.data = new HashMap<>();
+        }
+        return (T) this.data.get(key);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getData() {
+        return data;
+    }
+
 }
