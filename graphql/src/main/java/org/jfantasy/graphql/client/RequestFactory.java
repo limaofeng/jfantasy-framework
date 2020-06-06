0f4bf3aaa02bee77b54bbc15cda7deee196dc9cd
黄振泽 <18923623615@163.com>
2020-06-06 00:50:42 +0000

    加入Graphql接口调用工具类

---
+++ graphql/src/main/java/org/jfantasy/graphql/client/RequestFactory.java
@@ -0,0 +1,24 @@
+package org.jfantasy.graphql.client;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+
+public class RequestFactory {
+    private RequestFactory() {
+    }
+
+    static HttpEntity<Object> forJson(String json, HttpHeaders headers) {
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        return new HttpEntity<>(json, headers);
+    }
+
+    static HttpEntity<Object> forMultipart(String query, String variables, HttpHeaders headers) {
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        LinkedMultiValueMap<String, Object> values = new LinkedMultiValueMap<>();
+        values.add("query", forJson(query, new HttpHeaders()));
+        values.add("variables", forJson(variables, new HttpHeaders()));
+        return new HttpEntity<>(values, headers);
+    }
+}
