ffc71ac17184f715dd5859a51f4890316974cfdd
chenwj <12345678>
2020-10-10 11:46:27 +0800

    1：根据token从 Redis 中获取 用户信息

--- graphql/src/main/java/org/jfantasy/graphql/context/SecurityGraphQLContextBuilder.java
+++ graphql/src/main/java/org/jfantasy/graphql/context/SecurityGraphQLContextBuilder.java
@@ -9,6 +9,8 @@ import org.jfantasy.framework.security.DefaultSecurityContext;
 import org.jfantasy.framework.security.SecurityContextHolder;
 import org.jfantasy.framework.security.LoginUser;
 import org.jfantasy.framework.util.common.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Base64Utils;
 
@@ -17,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.websocket.Session;
 import javax.websocket.server.HandshakeRequest;
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -27,15 +30,17 @@ import java.util.concurrent.CompletableFuture;
  */
 @Component
 public class SecurityGraphQLContextBuilder implements GraphQLServletContextBuilder {
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Override
     public GraphQLContext build(HttpServletRequest req, HttpServletResponse response) {
         AuthorizationGraphQLServletContext context = new AuthorizationGraphQLServletContext(req, response);
         context.setDataLoaderRegistry(buildDataLoaderRegistry());
 
-        String authorization = req.getHeader("Authorization");
-        if(StringUtil.isNotBlank(authorization) && authorization.startsWith("token ")){
-            LoginUser user = JSON.deserialize(new String(Base64Utils.decodeFromString(authorization.replaceAll("^token ", ""))), LoginUser.class);
+        String token = req.getHeader("token");
+        if(StringUtil.isNotBlank(token)){
+            Object keys = redisTemplate.boundValueOps(token).get();
+            LoginUser user = JSON.deserialize(keys.toString(), LoginUser.class);
             SecurityContextHolder.setContext(new DefaultSecurityContext(user));
         }
         return context;
