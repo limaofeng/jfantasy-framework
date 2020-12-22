d0a0fc904390c7a9b4bda5bb4632fab6e15fb526
chenwj <12345678>
2020-12-22 14:25:53 +0800

    1：prod

--- graphql/src/main/java/org/jfantasy/graphql/context/SecurityGraphQLContextBuilder.java
+++ graphql/src/main/java/org/jfantasy/graphql/context/SecurityGraphQLContextBuilder.java
@@ -6,22 +6,17 @@ import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderRegistry;
 import org.jfantasy.framework.jackson.JSON;
 import org.jfantasy.framework.security.DefaultSecurityContext;
-import org.jfantasy.framework.security.LoginUser;
 import org.jfantasy.framework.security.SecurityContextHolder;
+import org.jfantasy.framework.security.LoginUser;
 import org.jfantasy.framework.util.common.StringUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Base64Utils;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.Session;
 import javax.websocket.server.HandshakeRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -32,8 +27,6 @@ import java.util.concurrent.CompletableFuture;
  */
 @Component
 public class SecurityGraphQLContextBuilder implements GraphQLServletContextBuilder {
-    @Autowired
-    private StringRedisTemplate redisTemplate;
 
     @Override
     public GraphQLContext build(HttpServletRequest req, HttpServletResponse response) {
@@ -42,12 +35,8 @@ public class SecurityGraphQLContextBuilder implements GraphQLServletContextBuild
 
         String authorization = req.getHeader("Authorization");
         if(StringUtil.isNotBlank(authorization) && authorization.startsWith("token ")){
-            String value = redisTemplate.boundValueOps(authorization.replaceAll("^token ","")).get();
-            if (!StringUtils.isEmpty(value)) {
-                Map<String, String> map = JSON.deserialize(value, HashMap.class);
-                LoginUser user = JSON.deserialize(JSON.serialize(map.get("user")), LoginUser.class);
-                SecurityContextHolder.setContext(new DefaultSecurityContext(user));
-            }
+            LoginUser user = JSON.deserialize(new String(Base64Utils.decodeFromString(authorization.replaceAll("^token ", ""))), LoginUser.class);
+            SecurityContextHolder.setContext(new DefaultSecurityContext(user));
         }
         return context;
     }
@@ -62,11 +51,11 @@ public class SecurityGraphQLContextBuilder implements GraphQLServletContextBuild
     private DataLoaderRegistry buildDataLoaderRegistry() {
         DataLoaderRegistry dataLoaderRegistry = new DataLoaderRegistry();
         dataLoaderRegistry.register("customerDataLoader",
-            new DataLoader<Integer, String>(customerIds ->
-                CompletableFuture.supplyAsync(() -> {
-                    System.out.println(customerIds);
-                    return new ArrayList<>();
-                })));
+                new DataLoader<Integer, String>(customerIds ->
+                        CompletableFuture.supplyAsync(() -> {
+                            System.out.println(customerIds);
+                            return new ArrayList<>();
+                        })));
         return dataLoaderRegistry;
     }
 
