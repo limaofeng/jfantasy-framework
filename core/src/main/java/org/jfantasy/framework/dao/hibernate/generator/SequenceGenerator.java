893bdff7d590047ff4905d3ebec94c474a37bcd9
李茂峰 <limaofeng@msn.com>
2021-03-18 19:38:05 +0800

    SequenceGenerator 如果存在 ID 就返回已设置的 ID

--- core/src/main/java/org/jfantasy/framework/dao/hibernate/generator/SequenceGenerator.java
+++ core/src/main/java/org/jfantasy/framework/dao/hibernate/generator/SequenceGenerator.java
@@ -2,10 +2,10 @@ package org.jfantasy.framework.dao.hibernate.generator;
 
 import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
-import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.Configurable;
 import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.type.Type;
 import org.jfantasy.framework.dao.mybatis.keygen.util.DataBaseKeyGenerator;
@@ -20,31 +20,37 @@ import java.util.Properties;
 
 /**
  * 自定义序列生成器
- * 
+ *
  * @author 李茂峰
- * @since 2013-1-14 下午02:07:25
  * @version 1.0
+ * @since 2013-1-14 下午02:07:25
  */
 public class SequenceGenerator implements IdentifierGenerator, Configurable {
 
-	@Autowired
-	private DataBaseKeyGenerator baseKeyGenerator;
+    @Autowired
+    private DataBaseKeyGenerator baseKeyGenerator;
 
-	public static final String KEY_NAME = "keyName";
+    public static final String KEY_NAME = "keyName";
 
-	private String keyName;
+    private String keyName;
 
 
-	@Override
-	public void configure(Type type, Properties params, ServiceRegistry serviceRegistry) throws MappingException {
-		this.keyName =  StringUtil.defaultValue(params.getProperty(KEY_NAME),params.getProperty("target_table")+":"+params.getProperty("target_column")).toLowerCase();
-	}
+    @Override
+    public void configure(Type type, Properties params, ServiceRegistry serviceRegistry) throws MappingException {
+        this.keyName = StringUtil.defaultValue(params.getProperty(KEY_NAME), params.getProperty("target_table") + ":" + params.getProperty("target_column")).toLowerCase();
+    }
 
-	@Override
-	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-		if (ObjectUtil.isNull(this.baseKeyGenerator)) {
-			SpringContextUtil.autowireBean(this);
-		}
-		return this.baseKeyGenerator.nextValue(StringUtil.defaultValue(keyName, ClassUtil.getRealClass(object).getName()));
-	}
+    @Override
+    public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+        Class entityClass = ClassUtil.getRealClass(object);
+        final EntityPersister persister = session.getFactory().getMetamodel().entityPersister(entityClass);
+        Serializable id = persister.getIdentifier(object, session);
+        if (id != null) {
+            return id;
+        }
+        if (ObjectUtil.isNull(this.baseKeyGenerator)) {
+            SpringContextUtil.autowireBean(this);
+        }
+        return this.baseKeyGenerator.nextValue(StringUtil.defaultValue(keyName, ClassUtil.getRealClass(object).getName()));
+    }
 }
