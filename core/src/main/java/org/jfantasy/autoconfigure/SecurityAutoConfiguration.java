6c0078c84117f03cc64f9d3a570ae6b3501c7df6
李茂峰 <limaofeng@msn.com>
2021-05-13 18:45:09 +0800

    自动装载 UserDetailsChecker 实现

--- core/src/main/java/org/jfantasy/autoconfigure/SecurityAutoConfiguration.java
+++ core/src/main/java/org/jfantasy/autoconfigure/SecurityAutoConfiguration.java
@@ -4,16 +4,21 @@ import org.jfantasy.framework.security.AuthenticationManager;
 import org.jfantasy.framework.security.authentication.AuthenticationEventPublisher;
 import org.jfantasy.framework.security.authentication.AuthenticationProvider;
 import org.jfantasy.framework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.jfantasy.framework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.jfantasy.framework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider.DefaultPreAuthenticationChecks;
+import org.jfantasy.framework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider.DefaultPostAuthenticationChecks;
 import org.jfantasy.framework.security.authentication.dao.DaoAuthenticationProvider;
-import org.jfantasy.framework.security.core.userdetails.UserDetailsService;
+import org.jfantasy.framework.security.core.userdetails.*;
 import org.jfantasy.framework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -40,11 +45,33 @@ public class SecurityAutoConfiguration {
         return authenticationManager;
     }
 
+    @Bean("pre.preUserDetailsCheckers")
+    public UserDetailsChecker preUserDetailsCheckers(PreUserDetailsChecker[] checkers) {
+        DefaultAuthenticationChecks checker = new DefaultAuthenticationChecks(new DefaultPreAuthenticationChecks());
+        checker.addCheckers(checkers);
+        return checker;
+    }
+
+    @Bean("post.preUserDetailsCheckers")
+    public UserDetailsChecker postUserDetailsCheckers(PostUserDetailsChecker[] checkers) {
+        DefaultAuthenticationChecks checker = new DefaultAuthenticationChecks(new DefaultPostAuthenticationChecks());
+        checker.addCheckers(checkers);
+        return checker;
+    }
+
     @Bean
     @ConditionalOnBean({UserDetailsService.class, PasswordEncoder.class})
     @ConditionalOnMissingBean({DaoAuthenticationProvider.class})
-    public DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        return new DaoAuthenticationProvider(userDetailsService, passwordEncoder);
+    public DaoAuthenticationProvider daoAuthenticationProvider(
+        UserDetailsService userDetailsService,
+        PasswordEncoder passwordEncoder,
+        @Qualifier("pre.preUserDetailsCheckers") UserDetailsChecker preUserDetailsCheckers,
+        @Qualifier("post.preUserDetailsCheckers") UserDetailsChecker postUserDetailsCheckers
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService, passwordEncoder);
+        provider.setPreAuthenticationChecks(preUserDetailsCheckers);
+        provider.setPostAuthenticationChecks(postUserDetailsCheckers);
+        return provider;
     }
 
 }
