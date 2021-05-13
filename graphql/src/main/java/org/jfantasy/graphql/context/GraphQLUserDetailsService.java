23faf55d4e27f6d7debe8dfc10570068ed48cb5e
李茂峰 <limaofeng@msn.com>
2021-05-13 17:05:47 +0800

    去掉冗余导包

--- graphql/src/main/java/org/jfantasy/graphql/context/GraphQLUserDetailsService.java
+++ graphql/src/main/java/org/jfantasy/graphql/context/GraphQLUserDetailsService.java
@@ -1,8 +1,6 @@
 package org.jfantasy.graphql.context;
 
 import org.jfantasy.framework.security.core.userdetails.SimpleUserDetailsService;
-import org.jfantasy.framework.security.core.userdetails.UserDetails;
-import org.jfantasy.framework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * GraphQL 身份获取服务
