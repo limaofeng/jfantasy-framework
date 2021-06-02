71a72ae28e1ceadb7dea18df643ca317f0d8635f
李茂峰 <limaofeng@msn.com>
2021-06-02 11:34:17 +0800

    修复逻辑错误

--- core/src/main/java/org/jfantasy/framework/security/oauth2/core/OAuth2Authentication.java
+++ core/src/main/java/org/jfantasy/framework/security/oauth2/core/OAuth2Authentication.java
@@ -15,20 +15,15 @@ import java.util.Collection;
 public class OAuth2Authentication extends AbstractAuthenticationToken {
 
     private OAuth2AuthenticatedPrincipal principal;
-    private OAuth2AuthenticationDetails credentials;
+    private Object credentials;
 
-    public OAuth2Authentication(Authentication authentication, OAuth2AuthenticationDetails credentials) {
+    public OAuth2Authentication(Authentication authentication, OAuth2AuthenticationDetails details) {
         super(authentication.getAuthorities());
-        Assert.isTrue(credentials.getTokenType() == OAuth2AccessToken.TokenType.BEARER, "credentials must be a bearer token");
+        Assert.isTrue(details.getTokenType() == OAuth2AccessToken.TokenType.BEARER, "credentials must be a bearer token");
+        this.credentials = authentication.getCredentials();
         this.principal = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
-        this.credentials = credentials;
+        setDetails(details);
         setAuthenticated(authentication.isAuthenticated());
-    }
-
-    public OAuth2Authentication(OAuth2AuthenticatedPrincipal principal, OAuth2AuthenticationDetails credentials, Collection<? extends GrantedAuthority> authorities) {
-        super(authorities);
-        Assert.isTrue(credentials.getTokenType() == OAuth2AccessToken.TokenType.BEARER, "credentials must be a bearer token");
-        setAuthenticated(true);
     }
 
     @Override
