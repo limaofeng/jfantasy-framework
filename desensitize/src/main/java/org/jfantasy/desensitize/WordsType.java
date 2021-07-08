739e75bac10505ec647ae795e4f324ee6c1d51a8
李茂峰 <limaofeng@msn.com>
2021-07-08 19:20:39 +0800

    添加 alex-sensitive-words-filter-3.0 重置版本

---
+++ desensitize/src/main/java/org/jfantasy/desensitize/WordsType.java
@@ -0,0 +1,6 @@
+package org.jfantasy.desensitize;
+
+public enum WordsType {
+    POSITIVE,
+    SENSITIVE
+}
---
+++ desensitize/src/main/java/org/jfantasy/desensitize/result/Word.java
@@ -0,0 +1,53 @@
+package org.jfantasy.desensitize.result;
+
+
+public class Word {
+    private int[] pos;
+    private int startPos;
+    private int endPos;
+    private String word;
+    private double level;
+
+    public Word() {
+    }
+
+    public int[] getPos() {
+        return this.pos;
+    }
+
+    public void setPos(int[] pos) {
+        this.pos = pos;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public double getLevel() {
+        return this.level;
+    }
+
+    public void setLevel(double level) {
+        this.level = level;
+    }
+
+    public int getStartPos() {
+        return this.startPos;
+    }
+
+    public void setStartPos(int startPos) {
+        this.startPos = startPos;
+    }
+
+    public int getEndPos() {
+        return this.endPos;
+    }
+
+    public void setEndPos(int endPos) {
+        this.endPos = endPos;
+    }
+}
