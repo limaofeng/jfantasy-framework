926fc449d7a6c410de67cbe604c969a0afda13a8
李茂峰 <limaofeng@msn.com>
2021-07-22 18:35:24 +0800

    fix: 准备添加 getEntityName 及 getTableName 方法

---
+++ core/src/test/java/org/jfantasy/framework/dao/hibernate/util/HibernateUtilsTest.java
@@ -0,0 +1,18 @@
+package org.jfantasy.framework.dao.hibernate.util;
+
+import org.junit.jupiter.api.Test;
+
+class HibernateUtilsTest {
+
+    @Test
+    public void getEntityName() throws Exception {
+    }
+
+    @Test
+    public void getTableName() throws Exception {
+    }
+
+    @Test
+    void getIdValue() {
+    }
+}
