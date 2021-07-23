db89bb6f230acf518eaeaa3765cc939219242734
李茂峰 <limaofeng@msn.com>
2021-07-23 09:52:21 +0800

    fix: 添加 getEntityName 及 getTableName 方法

--- core/src/main/java/org/jfantasy/framework/dao/hibernate/util/HibernateUtils.java
+++ core/src/main/java/org/jfantasy/framework/dao/hibernate/util/HibernateUtils.java
@@ -1,17 +1,22 @@
 package org.jfantasy.framework.dao.hibernate.util;
 
+import org.jfantasy.framework.error.ValidationException;
 import org.jfantasy.framework.util.common.ClassUtil;
+import org.jfantasy.framework.util.common.StringUtil;
 import org.jfantasy.framework.util.ognl.OgnlUtil;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 
 /**
+ * HibernateUtils 工具类
+ *
  * @author limaofeng
  * @version V1.0
- * @Description: TODO
  * @date 2019/8/23 10:34 上午
  */
 public class HibernateUtils {
@@ -28,10 +33,34 @@ public class HibernateUtils {
             for (Field idField : idFields) {
                 ognlUtil.setValue(idField.getName(), id, ognlUtil.getValue(idField.getName(), entity));
             }
-            return (ID)id;
+            return (ID) id;
         } else {
             return (ID) ClassUtil.getValue(entity, idFields[0].getName());
         }
+    }
+
+    public static <T> String getEntityName(Class<T> entityClass) {
+        Class clazz = ClassUtil.getRealClass(entityClass);
+        Entity entityAnnotation = ClassUtil.getAnnotation(clazz, Entity.class);
+        if (entityAnnotation == null) {
+            throw new ValidationException("未知实体:" + clazz.getName());
+        }
+        if (StringUtil.isBlank(entityAnnotation.name())) {
+            return clazz.getSimpleName();
+        }
+        return entityAnnotation.name();
+    }
+
+    public static <T> String getTableName(Class<T> entityClass) {
+        Class clazz = ClassUtil.getRealClass(entityClass);
+        Table entityAnnotation = ClassUtil.getAnnotation(clazz, Table.class);
+        if (entityAnnotation == null) {
+            throw new ValidationException("未知实体:" + clazz.getName());
+        }
+        if (StringUtil.isBlank(entityAnnotation.name())) {
+            return StringUtil.snakeCase(clazz.getSimpleName()).toUpperCase();
+        }
+        return entityAnnotation.name();
     }
 
 }
--- core/src/test/java/org/jfantasy/framework/dao/hibernate/util/HibernateUtilsTest.java
+++ core/src/test/java/org/jfantasy/framework/dao/hibernate/util/HibernateUtilsTest.java
@@ -1,15 +1,22 @@
 package org.jfantasy.framework.dao.hibernate.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jfantasy.framework.dao.mybatis.keygen.bean.Sequence;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class HibernateUtilsTest {
 
     @Test
     public void getEntityName() throws Exception {
+       String name = HibernateUtils.getEntityName(Sequence.class);
+       log.debug(name);
     }
 
     @Test
     public void getTableName() throws Exception {
+        String name = HibernateUtils.getTableName(Sequence.class);
+        log.debug(name);
     }
 
     @Test
