dd74dfb19519e8d6bf5c5cb88a80d54d04dffe6e
李茂峰 <limaofeng@msn.com>
2021-08-13 19:52:48 +0800

    ci: 添加测试覆盖率

---
+++ graphql/src/test/java/org/jfantasy/graphql/util/KitTest.java
@@ -0,0 +1,13 @@
+package org.jfantasy.graphql.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KitTest {
+
+    @Test
+    void connection() {
+        Kit.typeName(new Object());
+    }
+}
