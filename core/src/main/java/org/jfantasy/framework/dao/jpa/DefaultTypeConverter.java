19483ff68c95afbcf1dca3f74560734710d43a9e
李茂峰 <limaofeng@msn.com>
2024-03-04 13:47:31 +0800

    fix: 解决枚举类型查询BUG

--- core/src/main/java/org/jfantasy/framework/dao/jpa/DefaultTypeConverter.java
+++ core/src/main/java/org/jfantasy/framework/dao/jpa/DefaultTypeConverter.java
@@ -12,9 +12,12 @@ public class DefaultTypeConverter<T> implements TypeConverter<T> {
 
   @Override
   public T convert(Object value) {
+    if (type.isAssignableFrom(value.getClass())) {
+      return type.cast(value);
+    }
     if (type.isEnum()) {
-      //noinspection rawtypes
-      return (T) Enum.valueOf((Class<Enum>) type, (String) value);
+      //noinspection rawtypes,unchecked
+      return (T) Enum.valueOf((Class<Enum>) type, value.toString());
     }
     return ReflectionUtils.convert(value, type);
   }
